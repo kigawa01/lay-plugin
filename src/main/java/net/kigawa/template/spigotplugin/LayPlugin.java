7833e38088afde92d3376f2442d4eebe9217f7d2
kigawa <kigawa.8390@gmail.com>
2025-05-15 19:24:06 +0900

    Changes

---
+++ src/main/java/net/kigawa/template/spigotplugin/LayPlugin.java
@@ -0,0 +1,9 @@
+package net.kigawa.template.spigotplugin;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LayPlugin extends JavaPlugin
+{
+
+}
---
+++ src/main/java/net/kigawa/template/spigotplugin/command/LayCommand.java
@@ -0,0 +1,5 @@
+package net.kigawa.template.spigotplugin.command;
+
+public class LayCommand
+{
+}
